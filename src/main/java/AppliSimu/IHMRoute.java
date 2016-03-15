25036b53baa9413e965965257f6640288a175610
Milila <cannac.anais@hotmail.fr>
2016-03-15 11:41:59 +0100

    Deuxième commit (qui marche pas!)

--- src/main/java/AppliSimu/IHMRoute.java
+++ src/main/java/AppliSimu/IHMRoute.java
@@ -10,40 +10,51 @@ import java.util.Observer;
 /**
  * Created by Responsable on 15/03/2016.
  */
-public class IHMRoute extends JPanel implements Observer {
+public class IHMRoute extends JComponent {
     private Route route;
+    private JFrame frame;
+
 
     private void initGraphique() {
+
         this.setVisible(true);
+        repaint();
     }
 
-    public IHMRoute(Route maRoute) {
+    public IHMRoute(JFrame frame, Route maRoute) {
         super();
+        this.setSize(505, 505);
         this.route = maRoute;
-        maRoute.addObserver(this);
+        this.frame = frame;
+       // maRoute.addObserver(this);
         initGraphique();
+        //frame.add(this);
     }
 
-    public IHMRoute() {
+    public IHMRoute(JFrame frame) {
         super();
         initGraphique();
+        this.frame = frame;
         this.route = null;
+        //frame.add(this);
     }
-
+    /*@Override
     public void update(Observable o, Object arg) {
         this.repaint();
     }
-
+    */
     @Override
-    public void paint(Graphics contexteGraphique) {
-        super.paint(contexteGraphique);
-        contexteGraphique.setColor(Color.red);
+    public void paintComponent(Graphics contexteGraphique) {
+        //super.paint(contexteGraphique);
+
+        contexteGraphique.setColor(Color.BLACK);
         dessinerRoute(contexteGraphique);
     }
 
     private void dessinerRoute(Graphics contexteGraphique) {
 
-        contexteGraphique.fillRect((int)(route.getX()-(route.getLargeur()/2.0)), (int)(route.getY()-(route.getLargeur()/2.0)), 30, (int)route.getLargeur());
+        System.out.println("x " + (int)(route.getX()-(route.getLargeur()/2.0)) + "y " + (int)(route.getY()-(route.getLargeur()/2.0)));
+        contexteGraphique.fillRect((int)(route.getX()-(route.getLargeur()/2.0)), (int)(route.getY()-(route.getLargeur()/2.0)), 300, (int)route.getLargeur());
     }
 
 }
